9f2ba637afb61c9a37b0353f94ed35b18b70dbb2
vijay80 <vijay80@gmail.com>
2024-12-08 23:04:48 +0000

    Adding Gitlens extension for IDX

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,7 @@
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+    pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
 
@@ -19,6 +19,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "eamodio.gitlens"
     ];
 
     # Enable previews
